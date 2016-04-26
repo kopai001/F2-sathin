3bbfa0855862785ac61214d6011974ef6367f5b4
kopai001 <kopai-sathin@hotmail.com>
2016-04-26 11:25:37 +0700

    #01 สร้างLayoutที่มีbackgroundสีดำ

--- game/spw/frame.java
+++ game/spw/frame.java
@@ -1,3 +1,5 @@
+package game.spw;
+
 import javax.swing.*;
 import java.awt.*;
 public class frame {
@@ -5,9 +7,13 @@ public class frame {
 		JFrame frame = new JFrame("Space Game");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 200);
+		
+		frame.getContentPane().setLayout(new BorderLayout());
+		
+		GamePanel gp = new GamePanel();
+		frame.getContentPane().add(gp,BorderLayout.CENTER);
 		frame.setVisible(true);
 
-		JPanel facesPanel = new JPanel();
-		facesPanel.setBackground(Color.BLACK);
+
 	}
 }
